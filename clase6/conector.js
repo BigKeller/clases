ac2dbeda256f919f06afc5139dcb6be58145507b
BigKeller <pgonzalezk@ing.ucsc.cl>
2023-08-31 11:50:55 -0400

    clase 6 31-08

---
+++ clase6/conector.js
@@ -0,0 +1,9 @@
+function cambioazul(){
+    document.getElementById("azul").style.backgroundColor="blue";
+}
+function cambiocolor(){
+    document.getElementsByClassName("verde")[0].style.backgroundColor="green";
+}
+function reset(){
+    document.getElementById("azul").style.backgroundColor="red";
+}
